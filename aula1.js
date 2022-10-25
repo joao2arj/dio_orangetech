918406dcff4d3499804adddb1a4f1fc3f7ff584f
joao2arj <joao2arj@gmail.com>
2022-10-25 14:24:27 -0300

    add aula1 (var and math operators)

---
+++ aula1.js
@@ -0,0 +1,24 @@
+/*Para executar um arquivo .js no terminal = "node 'nome do arquivo'.js */
+
+//AULA 1
+
+//como declarar variáveis:
+
+let var1 = 10;        //variável que pode mudar seu valor conforme o código é escrito
+const pi = 3.14;        //'variável' constante, após ser declarada não pode mais alterar o seu valor, só pode ser declarada uma vez, após isso não serão mais aceitos outros valores
+var2 = 20;      //tambbém é possível declarar a variável diretamente na IDE
+
+console.log('Hello world!');        //a função console.log imprime algo na tela
+console.log(pi);
+
+
+//operadores matemáticos
+
+let var3 = 10 + 1 / 2 + 5;
+console.log(var3);
+
+
+
+
+
+
